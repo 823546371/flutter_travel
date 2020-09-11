8b704195f0ab80af5424c0add33a3b58aff77bd5
jwenfeng <823546371@qq.com>
2020-09-11 16:21:51 +0800

    done

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_travel/constants.dart';
 
 import 'widget/about.dart';
 import 'widget/attrabute.dart';
-import 'widget/attrabute_item.dart';
 import 'widget/book_now_button.dart';
 import 'widget/my_header.dart';
 import 'widget/place_and_name.dart';
